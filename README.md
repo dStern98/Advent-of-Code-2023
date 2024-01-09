dd6c14b89699384dab8cddaa3b6e63056f6a5dcd
DSTERN <david@mountainvector.com>
2024-01-09 00:35:50 -0700

    18 stars, day 19 completed with better comments

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-### Description
+### General Description
 
-I am working through the Advent of Code challenges for 2023, as can be found [here](https://adventofcode.com/2023). So far I have just used Rust, but may do some in Python at some point. I currently have 16 stars. Each day's challenge has two parts. Each day's solution can be found in that day's particular file (for example, day 15 would be found in `day15.rs`).
-Running `cargo run` will typically print the solution, provided the input file exists.
+I am working through the Advent of Code challenges for 2023, as can be found [here](https://adventofcode.com/2023). All solutions are either in Python or Rust. I currently have 18 stars. Each day's challenge has two parts. Each day's solution can be found in that day's particular file (for example, day 15 would be found in `day15.rs`).
+
+#### Rust Solutions
+
+For a day whose solutions is written in Rust, running `cargo run` will typically print the solution, provided the input file exists for Rust solutions, and that day's `SolveAdvent` methods are being invoked in `main.rs`.
 
 Each days solution must implement the trait:
 
@@ -15,3 +18,7 @@ trait SolveAdvent {
 ```
 
 where the two methods each take the file path to the input.
+
+#### Python Solutions
+
+If a solution is written in Python, then directly pass the file name to the interpreter to run it.
--- src/day19.rs
+++ src/day19.rs
@@ -23,32 +23,37 @@ impl SolveAdvent for Day19 {
                 }
             }
         }
-        println!("Total Part Ratings Number: {}", total_ratings_number);
+        println!(
+            "Total Accepted Part Ratings Numbers: {}",
+            total_ratings_number
+        );
     }
 
     fn solve_part2(path_to_file: &str) {
         let file_as_str = read_file_to_string(path_to_file);
         let (_, workflows) = process_into_workflows_ratings(file_as_str);
+        //Walk all possible workflows
         let mut parsed_workflows = traverse_workflows_recursively(&"in".to_owned(), &workflows);
+        //Because workflows are returned in the opposite order that they are traversed, we reverse all of the traversals.
         for workflow in parsed_workflows.iter_mut() {
             workflow.reverse();
         }
-        let accepted_paths = parsed_workflows
+        let total_acceptable_paths: usize = parsed_workflows
             .into_iter()
             .filter(|workflow| workflow.iter().last().unwrap() == &"A".to_owned())
-            .collect::<Vec<_>>();
-        let mut total_acceptable_paths = 0;
-        for acceptable_path in accepted_paths {
-            total_acceptable_paths += filter_valid_combinations(acceptable_path);
-        }
+            .map(|acceptable_path| count_valid_rating_combinations(acceptable_path))
+            .sum();
+
         println!(
-            "Total number of acceptable ratings: {}",
+            "Total number of unique acceptable ratings: {}",
             total_acceptable_paths
         );
     }
 }
 
 fn find_next_workflow(rating: &HashMap<String, usize>, workflow_rules: &Vec<String>) -> String {
+    //! Given a rating and a workflow's rules, return the destination (either A for accepted, R for Rejected, or
+    //! the key to another workflow).
     for rule in workflow_rules.iter() {
         if !rule.contains(":") {
             //If no colon is present, then the rule is itself a destination,
@@ -58,22 +63,18 @@ fn find_next_workflow(rating: &HashMap<String, usize>, workflow_rules: &Vec<Stri
         let mut rule_split = rule.split(":");
         let rule_actual = rule_split.next().unwrap();
         let destination = rule_split.next().unwrap();
-        if rule_actual.contains(">") {
-            let mut rule_actual_split = rule_actual.split(">");
-            let key_to_test = rule_actual_split.next().unwrap();
-            let number_to_test = rule_actual_split.next().unwrap().parse::<usize>().unwrap();
-            let rated_value = rating.get(key_to_test).unwrap();
-            if rated_value > &number_to_test {
-                return destination.to_owned();
-            }
-        } else if rule_actual.contains("<") {
-            let mut rule_actual_split = rule_actual.split("<");
-            let key_to_test = rule_actual_split.next().unwrap();
-            let number_to_test = rule_actual_split.next().unwrap().parse::<usize>().unwrap();
-            let actual_rated_value = rating.get(key_to_test).unwrap();
-            if actual_rated_value < &number_to_test {
-                return destination.to_owned();
-            }
+        let rule_symbol = if rule_actual.contains(">") { ">" } else { "<" };
+        let mut rule_actual_split = rule_actual.split(rule_symbol);
+        //Obtain the rule_key, rule_value to test against
+        //(for example x<256 means x is the key_to_test and 256 is the number_to_test)
+        let key_to_test = rule_actual_split.next().unwrap();
+        let number_to_test = rule_actual_split.next().unwrap().parse::<usize>().unwrap();
+        let rated_value = rating.get(key_to_test).unwrap();
+        //If the rated_value matches a logical rule, return that destination.
+        if rule_symbol == ">" && rated_value > &number_to_test {
+            return destination.to_owned();
+        } else if rule_symbol == "<" && rated_value < &number_to_test {
+            return destination.to_owned();
         }
     }
     panic!("Last rule contained a colon");
@@ -82,12 +83,15 @@ fn find_next_workflow(rating: &HashMap<String, usize>, workflow_rules: &Vec<Stri
 fn process_into_workflows_ratings(
     file_as_str: String,
 ) -> (Vec<HashMap<String, usize>>, HashMap<String, Vec<String>>) {
+    //! Given the input file as a String, process the ratings and workflows.
     let mut ratings = Vec::new();
     let mut workflows = Vec::new();
 
     for line in file_as_str.lines() {
+        //We know that all ratings start with a {
         if line.starts_with("{") {
             ratings.push(line)
+        //Ignore any empty lines.
         } else if line.len() > 0 {
             workflows.push(line);
         }
@@ -107,8 +111,7 @@ fn process_into_workflows_ratings(
             })
             .collect::<Vec<_>>();
 
-    let mut workflow_map = HashMap::new();
-
+    let mut workflow_map = HashMap::with_capacity(workflows.len());
     for workflow in workflows {
         let workflow = workflow.replace("}", "");
         let mut workflow_split = workflow.split("{");
@@ -137,29 +140,40 @@ fn traverse_workflows_recursively(
     } else if current_map_state == &"R".to_owned() {
         return vec![vec!["R".to_owned()]];
     } else {
+        //If none of the base cases is triggered, then recursively take each logical branch down the tree.
         let this_workflows_directions = workflows.get(current_map_state).unwrap();
         let mut outer_vec = Vec::new();
+        //Get the Inverses of each of the rules in this workflow.
         let rule_inverses = generate_inverse_of_rules(this_workflows_directions);
         for (index, direction) in this_workflows_directions.into_iter().enumerate() {
             if direction.contains(":") {
+                //An example direction is s>2770:qs
                 let this_directions_inverses = rule_inverses
                     .clone()
                     .drain(0..index)
                     .rev()
                     .collect::<Vec<_>>();
                 let mut current_workflow_direction_split = direction.split(":");
+                //In this example workflow rule is s>2770
                 let workflow_rule = current_workflow_direction_split.next().unwrap();
+                //In this example, go_to_node is qs
                 let go_to_node = current_workflow_direction_split.next().unwrap();
                 let mut next_nodes_response =
                     traverse_workflows_recursively(&go_to_node.to_string(), workflows);
+                //After the recursive call returns, add the workflow_rule that triggered the recursive call,
+                //as well as all of the applicable inverses.
                 for node in next_nodes_response.iter_mut() {
                     node.push(workflow_rule.to_owned());
                     node.extend(this_directions_inverses.clone());
                 }
                 outer_vec.extend(next_nodes_response);
             } else {
+                //Example direction: rfg. Here the go_to_node is the direction, so no direction parsing is necessary.
                 let mut next_nodes_response = traverse_workflows_recursively(direction, workflows);
                 for node in next_nodes_response.iter_mut() {
+                    //Because only the last clause in a workflow has no conditional, we know that
+                    // the inverse of all of the previous rules in this workflow must all be true to have
+                    //reached this point.
                     let mut rules_inverse = rule_inverses.clone();
                     rules_inverse.reverse();
                     node.extend(rules_inverse);
@@ -187,10 +201,12 @@ fn generate_inverse_of_rules(rules: &Vec<String>) -> Vec<String> {
         .collect::<Vec<_>>()
 }
 
-fn filter_valid_combinations(mut acceptable_branch: Vec<String>) -> usize {
+fn count_valid_rating_combinations(mut acceptable_branch: Vec<String>) -> usize {
+    //! Given an acceptable branch (a valid set of comparisons that reach A), return the unique
+    //! number of ratings that satisfy the branch requirements.
     //First, remove the "A" which we know must be at the end of the branch.
-
     acceptable_branch.pop().unwrap();
+    //Generate a HashMap with each rating type mapped to all 4000 possible rating values.
     let rating_range = (1..4001).collect::<Vec<u64>>();
     let mut allowed_combinations = HashMap::from([
         ("s".to_owned(), rating_range.clone()),
@@ -198,50 +214,39 @@ fn filter_valid_combinations(mut acceptable_branch: Vec<String>) -> usize {
         ("x".to_owned(), rating_range.clone()),
         ("m".to_owned(), rating_range),
     ]);
-    for rule in acceptable_branch {
-        let negate_truth_test = if rule.contains("!") { true } else { false };
-        let rule = rule.replace("!", "");
-        if rule.contains(">") {
-            let mut rule_iterator = rule.split(">");
-            let rule_key = rule_iterator.next().unwrap().to_owned();
-            let rule_value = rule_iterator.next().unwrap().parse::<u64>().unwrap();
-            let ratings_still_available = allowed_combinations.get_mut(&rule_key).unwrap();
-            let mut counter = 0;
-            while counter < ratings_still_available.len() {
-                let rating = ratings_still_available.get(counter).unwrap();
 
-                let truth_test = if negate_truth_test {
-                    !(rating <= &rule_value)
-                } else {
-                    rating <= &rule_value
-                };
-                if truth_test {
-                    ratings_still_available.remove(counter);
-                } else {
-                    counter += 1;
-                }
-            }
-        } else if rule.contains("<") {
-            let mut rule_iterator = rule.split("<");
-            let rule_key = rule_iterator.next().unwrap().to_owned();
-            let rule_value = rule_iterator.next().unwrap().parse::<u64>().unwrap();
-            let ratings_still_available = allowed_combinations.get_mut(&rule_key).unwrap();
-            let mut counter = 0;
-            while counter < ratings_still_available.len() {
-                let rating = ratings_still_available.get(counter).unwrap();
-                let truth_test = if negate_truth_test {
-                    !(rating >= &rule_value)
-                } else {
-                    rating >= &rule_value
-                };
-                if truth_test {
-                    ratings_still_available.remove(counter);
-                } else {
-                    counter += 1;
-                }
+    //Iterate over the rules in the acceptable branch
+    for rule in acceptable_branch {
+        //Parse the rule into a logical operation, a number, and a key.
+        let negate_truth_test = if rule.contains("!") { true } else { false };
+        let comparison_operation = if rule.contains(">") { ">" } else { "<" };
+        let rule = rule.replace("!", "");
+        let mut rule_iterator = rule.split(comparison_operation);
+        let rule_key = rule_iterator.next().unwrap().to_owned();
+        let rule_value = rule_iterator.next().unwrap().parse::<u64>().unwrap();
+        let rating_combinations_available = allowed_combinations.get_mut(&rule_key).unwrap();
+        //Filter out of that rating any value that the workflow would not accept.
+        let mut index = 0;
+        while index < rating_combinations_available.len() {
+            let rating = rating_combinations_available.get(index).unwrap();
+            let truth_test = if comparison_operation == ">" && negate_truth_test {
+                !(rating <= &rule_value)
+            } else if comparison_operation == ">" && !negate_truth_test {
+                rating <= &rule_value
+            } else if comparison_operation == "<" && negate_truth_test {
+                !(rating >= &rule_value)
+            } else {
+                rating >= &rule_value
+            };
+            if truth_test {
+                rating_combinations_available.remove(index);
+            } else {
+                index += 1;
             }
         }
     }
+    //Multiply the number of values in each of the 4 rating types to get the number of unique
+    //combinations.
     allowed_combinations
         .values()
         .map(|acceptable_range| acceptable_range.len())
