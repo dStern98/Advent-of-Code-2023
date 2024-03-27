a7789fd375f4c3a471668dd3765cf8ac88eb2b76
DSTERN <david@mountainvector.com>
2024-03-26 23:08:03 -0600

    37 Stars, Day14 Part1 done

--- advent_python/day14.py
+++ advent_python/day14.py
@@ -3,21 +3,44 @@ from .base import SolveAdvent
 
 class SolveDay14(SolveAdvent):
     def solve_part1(self):
-        rock_slider = RockSlider(self.file_content)
-        # rock_slider.pprint_map()
-        # rock_slider.slide_row_rocks_north(rock_slider.transposed_map[2])
-        rock_slider.slide_all_rocks_north()
+        RockSlider(self.file_content).slide_all_rocks_north()
 
     def solve_part2(self):
-        ...
+        # The current implementation is far too inefficient to have
+        # any hope of completing for the desired trillion iterations.
+        RockSlider(self.file_content).spin_cycle(1_000_000_000)
 
 
 class RockSlider:
     def __init__(self, file_content: list[str]):
+        """
+        :param map: the platform of rocks as described in the platform.
+        """
         self.map = [[char for char in row if char != "\n"]
                     for row in file_content]
 
+    @property
+    def north_support_load(self) -> int:
+        """
+        The total load on the north support beam based
+        on the current `map`
+        """
+        total_load = 0
+        platform_height = len(self.map)
+        for index, row in enumerate(self.map):
+            score_per = platform_height - index
+            number_of_round_rocks = len([item for item in row if item == "O"])
+            total_load += number_of_round_rocks * score_per
+        return total_load
+
+    def flip_map_180(self):
+        for row in self.map:
+            row.reverse()
+
     def transpose_map(self):
+        """
+        Perform a transpose opperation on the map, so that rows and columns flip.
+        """
         transposed_content = [["" for _ in range(
             len(self.map))] for _ in range(len(self.map[0]))]
         for row_number in range(len(self.map)):
@@ -26,11 +49,18 @@ class RockSlider:
         self.map = transposed_content
 
     def pprint_map(self):
+        """
+        Print the map as its shown in the problem prompt.
+        """
         print("\n".join(("".join(row) for row in self.map)))
 
     @staticmethod
-    def slide_row_rocks_north(row: list[str]):
-        # copied_row = [item for item in row]
+    def slide_row_rocks_west(row: list[str]):
+        """
+        In place operation to slide all rocks west as
+        far as they will go. Only circular rocks `O` slide,
+        cube-shaped rocks `#` do not slide.
+        """
         for current_position, rock in enumerate(row):
             if rock != ".":
                 continue
@@ -44,26 +74,52 @@ class RockSlider:
                 elif next_rock == "#":
                     break
                 explorer_index += 1
-        # print("------------------------------")
-        # print(f"Row Before: {copied_row}")
-        # print(f"Row after: {row}")
+
+    def slide_all_rocks_west(self):
+        for row in self.map:
+            self.slide_row_rocks_west(row)
 
     def slide_all_rocks_north(self):
         """
-        ...
+        Use the `slide_all_rocks_west` method, but transpose
+        the map before and after sliding the rocks west. The end result
+        is equivalent to sliding all of the rocks North, albeit
+        not optimally
         """
         self.transpose_map()
-        # self.pprint_map()
-        for row in self.map:
-            self.slide_row_rocks_north(row)
+        self.slide_all_rocks_west()
         self.transpose_map()
-        # self.pprint_map()
+        print(f"Total Load on North Platform is {self.north_support_load}")
 
-        total_load = 0
-        platform_height = len(self.map)
-        for index, row in enumerate(self.map):
-            score_per = platform_height - index
-            number_of_round_rocks = len([item for item in row if item == "O"])
-            total_load += number_of_round_rocks * score_per
+    def spin_cycle(self, number_of_cycles: int):
+        """
+        As directed in Part2, Spin the map by first moving
+        North, West, South, and East.
 
-        print(f"Total Load on North Platform is {total_load}")
+        The current implementation is extremely inefficient because
+        of all of the tranposes and 180 operations. A better version
+        would directly implement the sliding logic for all 4 directions,
+        """
+        for cycle in range(number_of_cycles):
+            if cycle % 1_000 == 0:
+                print(f"Reached cycle {cycle}/{number_of_cycles}")
+            # First, roll all of the rocks North
+            self.transpose_map()
+            self.slide_all_rocks_west()
+            self.transpose_map()
+
+            # Now roll west
+            self.slide_all_rocks_west()
+
+            # Roll South
+            self.transpose_map()
+            self.flip_map_180()
+            self.slide_all_rocks_west()
+            self.flip_map_180()
+            self.transpose_map()
+
+            # Roll East
+            self.flip_map_180()
+            self.slide_all_rocks_west()
+            self.flip_map_180()
+        print(f"Load on North Support Beam: {self.north_support_load}")
